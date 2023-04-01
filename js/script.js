9c482a299535e13e746c0f3f8cf76307c783d1d3
Lupinewolf <proyectodeprogramador@gmail.com>
2023-04-01 20:47:44 -0300

    Menu hamburguesa  y sidebar actualizada con js
    Agregué el menú hamburguesa para desplegar la barra lateral en conjunto con le script necesario

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
-let usuario = "Mundo";
-alert("Hola " + usuario);
+const hamburguer = document.querySelector(".hamburguer");
+const menu = document.querySelector(".sidebar");
+
+//console.log(menu)
+//console.log(hamburguer)
+
+hamburguer.addEventListener("click", () => {
+  menu.classList.toggle("aparecer"); //cada vez que hagamos click, al menú hamburguesa, le va agregar la clase aparecer
+});
+
+window.addEventListener("click", (e) => {
+  if (
+    menu.classList.contains("aparecer") &&
+    e.target != menu &&
+    e.target != hamburguer
+  ) {
+    menu.classList.toggle("aparecer");
+  }
+
+  console.log(e.target); //me dice a qué elemento le doy click
+});
