74b6e672bb5f96665663fedebd7a52c71142fd6d
Lupinewolf <proyectodeprogramador@gmail.com>
2023-05-20 14:54:41 -0300

    Revert "INTEGRACIÓN DE API"
    This reverts commit ee9513ff228f38256e4756525a3c88f7506b7198.

--- js/validacionForm.js
+++ js/validacionForm.js
@@ -1,76 +1,42 @@
-// Importa la biblioteca EmailJS SDK
-import emailjs from "emailjs-com";
-
-// Configura EmailJS con tu información de usuario y servicio
-emailjs.init("vNzv-BySxyX5h1YCfgZUi");
-
-// Función para enviar el formulario y validar los campos
-function enviarFormulario(event) {
-  event.preventDefault(); // Evita que se envíe el formulario por defecto
-
-  // Validar el formulario antes de enviarlo
-  if (!validarFormulario()) {
-    return;
-  }
-
-  // Obtén los valores de los campos del formulario
-  const nombre = document.getElementById("nombre").value;
-  const apellido = document.getElementById("apellido").value;
-  const correo = document.getElementById("correo").value;
-  const celular = document.getElementById("cel").value;
-  const comentario = document.getElementById("comentario").value;
-
-  // Crea un objeto con los datos del formulario
-  const datosFormulario = {
-    from_name: nombre + " " + apellido,
-    from_email: correo,
-    message: comentario,
-  };
-
-  // Envía el correo electrónico utilizando EmailJS
-  emailjs
-    .send("service_w00ldb8", "template_jg1803u", datosFormulario)
-    .then(function (response) {
-      console.log(
-        "Correo electrónico enviado:",
-        response.status,
-        response.text
-      );
-      alert("Formulario enviado correctamente.");
-      document.getElementById("form").reset(); // Restablece el formulario después del envío
-    })
-    .catch(function (error) {
-      console.error("Error al enviar el correo electrónico:", error);
-      alert(
-        "Hubo un error al enviar el formulario. Por favor, inténtalo nuevamente más tarde."
-      );
-    });
-}
-
 function validarFormulario() {
-  const nombre = document.getElementById("nombre").value.trim();
-  const apellido = document.getElementById("apellido").value.trim();
-  const correo = document.getElementById("correo").value.trim();
-  const celular = document.getElementById("cel").value.trim();
+  let nombre = document.querySelector("#nombre").value.trim();
+  let apellido = document.querySelector("#apellido").value.trim();
+  let email = document.querySelector("#email").value.trim();
+  let celular = document.querySelector("#cel").value.trim();
 
-  if (nombre === "" || apellido === "" || correo === "" || celular === "") {
-    alert("Por favor, complete todos los campos del formulario.");
+  if (nombre === "" || apellido === "" || email === "" || celular === "") {
+    alert("Por favor complete todos los campos del formulario");
     return false;
   }
 
-  const nombreRegExp = /^[a-zA-Z\s]+$/;
-  if (!nombreRegExp.test(nombre)) {
-    alert(
-      "El campo 'Nombre' solo puede contener caracteres alfabéticos y espacios."
-    );
-    return false;
+  for (var i = 0; i < nombre.length; i++) {
+    var charCode = nombre.charCodeAt(i);
+    if (
+      !(
+        (charCode >= 65 && charCode <= 90) ||
+        (charCode >= 97 && charCode <= 122) ||
+        charCode === 32
+      )
+    ) {
+      alert(
+        "El campo 'nombre' solo puede contener caracteres alfabéticos y espacios."
+      );
+      return false;
+    }
   }
 
-  const celularRegExp = /^\d{8,12}$/;
-  if (!celularRegExp.test(celular)) {
-    alert("Por favor, ingrese un número de teléfono válido (8 a 12 dígitos).");
+  if (!(celular.length > 7 && celular.length < 13)) {
+    alert("El número de teléfono debe ser válido");
     return false;
   }
+  for (var j = 0; j < celular.length; j++) {
+    let digito = celular.charAt(j);
+    if (digito < "0" || digito > "9") {
+      alert("El campo 'dni' solo puede contener dígitos numéricos.");
+      return false;
+    }
+  }
 
+  alert("Formulario enviado correctamente.");
   return true;
 }
