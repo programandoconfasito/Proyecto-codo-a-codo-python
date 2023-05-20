e761fda3a61c26e765c8805ea92b3c86b1778576
Lupinewolf <proyectodeprogramador@gmail.com>
2023-05-20 14:54:34 -0300

    Revert "asdasd"
    This reverts commit ebc44ca689bd25cdf9f706e4034d7c582852472e.

--- js/validacionForm.js
+++ js/validacionForm.js
@@ -1,51 +1,51 @@
-// // Importa la biblioteca EmailJS SDK
-// import emailjs from "emailjs-com";
+// Importa la biblioteca EmailJS SDK
+import emailjs from "emailjs-com";
 
-// // Configura EmailJS con tu información de usuario y servicio
-// emailjs.init("vNzv-BySxyX5h1YCfgZUi");
+// Configura EmailJS con tu información de usuario y servicio
+emailjs.init("vNzv-BySxyX5h1YCfgZUi");
 
-// // Función para enviar el formulario y validar los campos
-// function enviarFormulario(event) {
-//   event.preventDefault(); // Evita que se envíe el formulario por defecto
+// Función para enviar el formulario y validar los campos
+function enviarFormulario(event) {
+  event.preventDefault(); // Evita que se envíe el formulario por defecto
 
-//   // Validar el formulario antes de enviarlo
-//   if (!validarFormulario()) {
-//     return;
-//   }
+  // Validar el formulario antes de enviarlo
+  if (!validarFormulario()) {
+    return;
+  }
 
-//   // Obtén los valores de los campos del formulario
-//   const nombre = document.getElementById("nombre").value;
-//   const apellido = document.getElementById("apellido").value;
-//   const correo = document.getElementById("correo").value;
-//   const celular = document.getElementById("cel").value;
-//   const comentario = document.getElementById("comentario").value;
+  // Obtén los valores de los campos del formulario
+  const nombre = document.getElementById("nombre").value;
+  const apellido = document.getElementById("apellido").value;
+  const correo = document.getElementById("correo").value;
+  const celular = document.getElementById("cel").value;
+  const comentario = document.getElementById("comentario").value;
 
-//   // Crea un objeto con los datos del formulario
-//   const datosFormulario = {
-//     from_name: nombre + " " + apellido,
-//     from_email: correo,
-//     message: comentario,
-//   };
+  // Crea un objeto con los datos del formulario
+  const datosFormulario = {
+    from_name: nombre + " " + apellido,
+    from_email: correo,
+    message: comentario,
+  };
 
-//   // Envía el correo electrónico utilizando EmailJS
-//   emailjs
-//     .send("service_w00ldb8", "template_jg1803u", datosFormulario)
-//     .then(function (response) {
-//       console.log(
-//         "Correo electrónico enviado:",
-//         response.status,
-//         response.text
-//       );
-//       alert("Formulario enviado correctamente.");
-//       document.getElementById("form").reset(); // Restablece el formulario después del envío
-//     })
-//     .catch(function (error) {
-//       console.error("Error al enviar el correo electrónico:", error);
-//       alert(
-//         "Hubo un error al enviar el formulario. Por favor, inténtalo nuevamente más tarde."
-//       );
-//     });
-// }
+  // Envía el correo electrónico utilizando EmailJS
+  emailjs
+    .send("service_w00ldb8", "template_jg1803u", datosFormulario)
+    .then(function (response) {
+      console.log(
+        "Correo electrónico enviado:",
+        response.status,
+        response.text
+      );
+      alert("Formulario enviado correctamente.");
+      document.getElementById("form").reset(); // Restablece el formulario después del envío
+    })
+    .catch(function (error) {
+      console.error("Error al enviar el correo electrónico:", error);
+      alert(
+        "Hubo un error al enviar el formulario. Por favor, inténtalo nuevamente más tarde."
+      );
+    });
+}
 
 function validarFormulario() {
   const nombre = document.getElementById("nombre").value.trim();
