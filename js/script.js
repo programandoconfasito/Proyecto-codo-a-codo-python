2a8dcc068c9808ba98ea8176b438553711e635a7
Lupinewolf <proyectodeprogramador@gmail.com>
2023-07-09 20:23:00 -0300

    cambios...

--- js/script.js
+++ js/script.js
@@ -37,31 +37,25 @@ window.addEventListener("touch", (e) => {
 });
 
 // Agregar celulares al carrito
-const URL = "http://lupinewolf.pythonanywhere.com/"
+const URL = "http://lupinewolf.pythonanywhere.com/";
 
 function agregarAlCarrito(codigo) {
-  fetch(URL + 'carrito', {
-    method: 'POST',
+  fetch(URL + "carrito", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       codigo: codigo,
       cantidad: 1, // Puedes ajustar esto según tus necesidades
     }),
   })
-    .then(response => response.json())
-    .then(data => {
+    .then((response) => response.json())
+    .then((data) => {
       alert(data.message);
     })
-    .catch(error => {
-      console.error('Error al agregar el producto al carrito:', error);
-      alert('Error al agregar el producto al carrito.');
+    .catch((error) => {
+      console.error("Error al agregar el producto al carrito:", error);
+      alert("Error al agregar el producto al carrito.");
     });
 }
-
-
-
-
-
-
